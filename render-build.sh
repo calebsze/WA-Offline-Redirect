07d22c00be482e4314ae47750d464510ca4c4b46
Caleb <calebszekailok@gmail.com>
2026-09-15 18:41:24 +0800

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
-
 npm install
-# Force puppeteer to install the working chrome binary in the cloud instance
 npx puppeteer browsers install chrome
